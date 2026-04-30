a2025003cde6da99ccb52be31a871f6d7abec53b
Abi Shrestha <brawlershere@gmail.com>
2026-04-30 13:53:32 +0545

    idk

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite-plus";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   lint: { options: { typeAware: true, typeCheck: true } },
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), react()],
 });
